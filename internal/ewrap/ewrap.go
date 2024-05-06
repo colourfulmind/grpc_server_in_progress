1201f2909b4f41d1e252bc4138fa78a3fbc41189
Merrill Yvette <merrilly@at-b2.msk.21-school.ru>
2024-05-06 18:39:09 +0300

    register user server part

--- internal/ewrap/ewrap.go
+++ internal/ewrap/ewrap.go
@@ -1,1 +1,14 @@
 package ewrap
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrEmailRequired    = status.Error(codes.InvalidArgument, "email is required")
+	ErrPasswordRequired = status.Error(codes.InvalidArgument, "password is required")
+	UserAlreadyExists   = status.Error(codes.AlreadyExists, "user already exists")
+	ErrConnectionTime   = status.Error(codes.DeadlineExceeded, "reached timeout while connecting to the database")
+	InternalError       = status.Error(codes.Internal, "internal error")
+)
--- internal/grpc/sso/server.go
+++ internal/grpc/sso/server.go
@@ -2,12 +2,19 @@ package sso
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"main/internal/ewrap"
+	"main/internal/services/sso"
+	"main/pkg/logger/sl"
 	server "main/protos/gen/go/blog"
 )
 
 type ServerSSO struct {
 	server.UnimplementedSSOServer
 	sso SSO
+	log *slog.Logger
 }
 
 type SSO interface {
@@ -16,8 +23,52 @@ type SSO interface {
 	IsAdmin(ctx context.Context, userID int64) (bool, error)
 }
 
-func RegisterNewUser(ctx context.Context, req *server.RegisterRequest) (*server.RegisterResponse, error) {
-	panic("not implemented")
+func Register(s *grpc.Server, sso SSO, log *slog.Logger) {
+	server.RegisterSSOServer(s, &ServerSSO{
+		sso: sso,
+		log: log,
+	})
+}
+
+func (s *ServerSSO) RegisterNewUser(ctx context.Context, req *server.RegisterRequest) (*server.RegisterResponse, error) {
+	const op = "internal.grpc.sso.RegisterNewUser"
+
+	log := s.log.With(slog.String("op", op), slog.String("email", req.GetEmail()))
+	log.Info("register new user")
+
+	if err := ValidateCredentials(req); err != nil {
+		log.Error("invalid email or password", sl.Err(err))
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	userID, err := s.sso.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		if errors.Is(err, sso.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+			return nil, ewrap.UserAlreadyExists
+		}
+		if errors.Is(err, sso.ErrConnectionTime) {
+			log.Error("failed to connect to the database", sl.Err(err))
+			return nil, ewrap.ErrConnectionTime
+		}
+		return nil, ewrap.InternalError
+	}
+
+	return &server.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func ValidateCredentials(req *server.RegisterRequest) error {
+	// TODO: regex to check the email
+	if req.GetEmail() == "" {
+		return ewrap.ErrEmailRequired
+	}
+	// TODO: minimum password length
+	if req.GetPassword() == "" {
+		return ewrap.ErrPasswordRequired
+	}
+	return nil
 }
 
 func Login(ctx context.Context, req *server.LoginRequest) (*server.LoginResponse, error) {
