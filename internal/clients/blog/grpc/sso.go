e211d14b8296660c74a84737e894c9a67b68fae9
Merrill Yvette <merrilly@at-b2.msk.21-school.ru>
2024-05-11 16:41:32 +0300

    check on error in grpc/sso.go

--- internal/clients/blog/grpc/sso.go
+++ internal/clients/blog/grpc/sso.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"main/internal/domain/models"
+	"main/pkg/logger/sl"
 	server "main/protos/gen/go/blog"
 	"net/http"
 )
@@ -16,38 +17,34 @@ func (c *Client) RegisterNewUser(ginCtx *gin.Context) {
 	if err := ginCtx.BindJSON(&user); err != nil {
 		return
 	}
-	ginCtx.IndentedJSON(http.StatusCreated, user)
+
+	c.log.Info("user", user)
 
 	resp, err := c.sso.RegisterNewUser(context.Background(), &server.RegisterRequest{
 		Email:    user.Email,
 		Password: user.Password,
 	})
 
+	// "error": "rpc error: code = Unavailable desc = connection error: desc = \"error reading server preface: http2: frame too large\""
 	if err != nil {
+		c.log.Error("user was not created", sl.Err(err))
 		return
 	}
-
-	c.log.Info("user is created")
 	ginCtx.IndentedJSON(http.StatusCreated, resp.UserId)
 }
 
 func (c *Client) GetUser(ginCtx *gin.Context) {
-	var user models.NewUser
-
-	// TODO: return error
-	if err := ginCtx.BindJSON(&user); err != nil {
-		return
-	}
+	var user = models.NewUser{Email: "test@test.test", Password: "1234"}
 
 	resp, err := c.sso.Login(context.Background(), &server.LoginRequest{
 		Email:    user.Email,
 		Password: user.Password,
 	})
+
 	if err != nil {
 		ginCtx.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 		return
 	}
 
-	c.log.Info("user is gotten")
 	ginCtx.IndentedJSON(http.StatusOK, resp)
 }
--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -2,13 +2,13 @@ package models
 
 // User is a struct to store information about user
 type User struct {
-	ID       int64
-	Email    string
-	PassHash []byte
-	IsAdmin  bool
+	ID       int64  `json:"id"`
+	Email    string `json:"email"`
+	PassHash []byte `json:"pass_hash"`
+	IsAdmin  bool   `json:"is_admin"`
 }
 
 type NewUser struct {
-	Email    string
-	Password string
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
